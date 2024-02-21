11b305a2e90acc442460f66b83ef55c01c227ead
RudiySVR <rudiyserj@gmail.com>
2024-02-21 19:48:16 +0200

    Del bat

--- Tetris.bat
+++
@@ -1,7 +0,0 @@
-echo "# tetris" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/RudiySVR/tetris.git
-git push -u origin main
